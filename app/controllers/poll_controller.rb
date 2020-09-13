e3ffe85204140ac571e0ae5807feb543c0ca19ac
Chetan-Gemini <ctomar@gemini-us.com>
2020-09-13 21:38:17 +0530

    Update controller and some views

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -8,11 +8,19 @@ class PollController < ApplicationController
 	def submitt
     answer = Answer.find(params[:answer_id])
     question_id = answer.question.id
-    
+    is_updated = false
     unless current_user.user_questions.exists?(user_id: current_user.id, question_id: question_id)
-       UserQuestion.create({ user_id: current_user.id, question_id: question_id})
-       answer.update(total: answer.total + 1)
+      UserQuestion.create({ user_id: current_user.id, question_id: question_id})
+      is_updated = answer.update(total: answer.total + 1)
     end
-    redirect_to poll_path
-	end
+    @questions = Question.all
+    respond_to do |format|
+      if is_updated
+        format.html { redirect_to poll_path, notice: 'Poll was successfully submitted.' }
+      else
+        flash.now[:alert] = 'You have already submitted this poll.'
+        format.html { render :index }
+      end
+    end
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
 	has_many :answers, inverse_of: :question, dependent: :destroy
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+  validates :title, presence: true
 end
