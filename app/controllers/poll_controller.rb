875db19f6bb9a9461645b90f83300079e77898fb
Sakshi7842 <ssingh.sakshi7898@gmail.com>
2020-09-13 23:55:54 +0530

    dynamic poll

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -8,11 +8,22 @@ class PollController < ApplicationController
 	def submitt
     answer = Answer.find(params[:answer_id])
     question_id = answer.question.id
-    
+    is_answered = false
     unless current_user.user_questions.exists?(user_id: current_user.id, question_id: question_id)
-       UserQuestion.create({ user_id: current_user.id, question_id: question_id})
+      is_answered = UserQuestion.create({ user_id: current_user.id, question_id: question_id})
        answer.update(total: answer.total + 1)
     end
-    redirect_to poll_path
+
+    @questions = Question.all
+
+    respond_to do |format|
+      if is_answered
+        format.html { redirect_to poll_path, notice: 'You have submitted poll of this question.' }
+      else
+        flash.now[:alert] = 'You have already polled.'
+        format.html { render :index }
+      end
+    end
 	end
+
 end
